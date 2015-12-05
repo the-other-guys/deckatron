e58aa6ac548d09d631968a9f92db4b36c526e06a
Nikita Prokopov <prokopov@gmail.com>
2015-12-05 15:05:46 +0600

    use only lowercase letters in ssid because filenames

--- src/deckatron/util.clj
+++ src/deckatron/util.clj
@@ -5,18 +5,18 @@
 
 (defn ssid
   "Generate semi-sequential id based on current time and 22 bit random number,
-   encoded as 11-char 'safe' ASCII string. E.g. (ssid) => 'FRcSwaV0Bkv'"
+   encoded as 13-char 'safe' ASCII string. E.g. (ssid) => 'fixcyytw1x0wk'"
   []
   (let [id    (-> (.getTime (java.util.Date.))
                   (bit-shift-left 22)
                   (bit-or (rand-int 0x800000)))
-        table "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"]
+        table "abcdefghijklmnopqrstuvwxyz0123456"]
     (loop [id id
            res ""]
       (if (== 0 id)
         res
-        (let [digit (bit-and id 0x3F)]
-          (recur (bit-shift-right id 6) (str (nth table digit) res)))))))
+        (let [digit (bit-and id 0x1F)]
+          (recur (bit-shift-right id 5) (str (nth table digit) res)))))))
 
 
 (defn die  [message map] (throw (ex-info message map)))
