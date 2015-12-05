6b9a69970f334bda19266319ac485c1332cfe5b1
Misha Akovantsev <mikhail.akovantsev@gmail.com>
2015-12-05 05:45:00 +0200

    add some markdown(?) grammar.

--- src/deckatron/parser.cljs
+++ src/deckatron/parser.cljs
@@ -1,1 +1,32 @@
-(ns deckatron.parser)
+(ns deckatron.parser
+  (:require
+    [instaparse.core :as insta]))
+
+
+(def ^:private parser
+  ;;https://github.com/chameco/Hitman/blob/master/src/hitman/core.clj#L9
+  (insta/parser
+    "<Blocks> = (Paragraph | Header | List | Ordered | Code | Rule)+
+    Header = Line Headerline Blankline+
+    <Headerline> = h1 | h2
+    h1 = '='+
+    h2 = '-'+
+    List = Listline+ Blankline+
+    Listline = Listmarker Whitespace+ Word (Whitespace Word)* EOL
+    <Listmarker> = <'+' | '*' | '-'>
+    Ordered = Orderedline+ Blankline+
+    Orderedline = Orderedmarker Whitespace* Word (Whitespace Word)* EOL
+    <Orderedmarker> = <#'[0-9]+\\.'>
+    Code = Codeline+ Blankline+
+    Codeline = <Space Space Space Space> (Whitespace | Word)* EOL
+    Rule = Ruleline Blankline+
+    <Ruleline> = <'+'+ | '*'+ | '-'+>
+    Paragraph = Line+ Blankline+
+    <Blankline> = Whitespace* EOL
+    <Line> = Linepre Word (Whitespace Word)* Linepost EOL
+    <Linepre> = (Space (Space (Space)? )? )?
+    <Linepost> = Space?
+    <Whitespace> = #'(\\ | \\t)+'
+    <Space> = ' '
+    <Word> = #'\\S+'
+    <EOL> = <'\\n'>"))
