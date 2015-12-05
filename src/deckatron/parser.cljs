5a64b27913087fa7f2f0595f7ff5588d49557d25
Misha Akovantsev <mikhailakovantsev@gmail.com>
2015-12-05 05:50:19 +0200

    make span rules private.

--- src/deckatron/parser.cljs
+++ src/deckatron/parser.cljs
@@ -35,7 +35,7 @@
   [s & types]
   {:text s :e/types (set types)})
 
-(def SPAN-RULES
+(def ^:private SPAN-RULES
   ;;https://github.com/chameco/Hitman/blob/master/src/hitman/core.clj#L36
   [[#"`(\S+)`"             (fn [s] (->element s :code))]
    [#"\*\*(\S+)\*\*"       (fn [s] (->element s :strong))]
